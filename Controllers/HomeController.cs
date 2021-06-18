f6d706f026de8878dd490bf1156b1181ae7316e8
Listerine87 <brettjmill@gmail.com>
2021-06-17 19:20:21 -0700

    Initial commit for Login/Registration assignment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LoginReg.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace LoginReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LoginRegContext db;
+        public HomeController(LoginRegContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Register()
+        {
+            if(HttpContext.Session.GetInt32("UserId") != null)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return View("Register");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult SubmitRegistration(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(db.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+            }
+
+            if(ModelState.IsValid == false)
+            {
+                return View("Register");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FirstName", newUser.FirstName);
+
+            return RedirectToAction("Success");
+        }
+
+        [HttpGet("/loginPage")]
+        public IActionResult LoginPage()
+        {
+            if(HttpContext.Session.GetInt32("UserId") != null)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return View("Login");
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return View("Login");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+            if(dbUser == null)
+            {
+                ModelState.AddModelError("LoginEmail", "Invalid email/password.");
+                return View("Login");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if(pwCompareResult == 0)
+            {
+                ModelState.AddModelError("LoginEmail", "Invalid email/password.");
+                return View("Login");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FirstName", dbUser.FirstName);
+
+            return RedirectToAction("Success");
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Register");
+        }
+
+        [HttpGet("/success")]
+        public IActionResult Success()
+        {
+            if(HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Register");
+            }
+
+            return View("Success");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
